71fba63cb164679a66fd16ce0e7c6496a5d5d73d
lesterietado <leandrocabrera7c@gmail.com>
2025-10-15 15:29:23 -0300

    delete al componente contador y agregar el contador al componente preguntas. hacer lo mismo con las otras preguntas

--- Quiz/src/app/app.ts
+++ Quiz/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Contador } from './pages/contador/contador';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Contador],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css'] // Corregido aquí
 })
--- Quiz/src/app/pages/preguntas/preguntas.ts
+++ Quiz/src/app/pages/preguntas/preguntas.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-preguntas',
+  selector: 'preguntas',
+  standalone: true,
   imports: [],
   templateUrl: './preguntas.html',
-  styleUrl: './preguntas.css'
+  styleUrls: ['./preguntas.css']
 })
-export class Preguntas {
+export class Preguntas implements OnInit {
+  minutos: number = 0;
+  segundos: number = 7;
+  private intervalo: any;
 
+  constructor(private cdr: ChangeDetectorRef, private router: Router) {}
+
+  ngOnInit() {
+    this.iniciarContador();
+  }
+
+  iniciarContador() {
+    this.intervalo = setInterval(() => {
+      if (this.segundos === 0) {
+        if (this.minutos === 0) {
+          this.router.navigate(['/perdiste']); // Redirige al componente perdiste
+          clearInterval(this.intervalo);
+          
+        } else {
+          this.minutos--;
+          this.segundos = 59;
+        }
+      } else {
+        this.segundos--;
+      }
+      this.cdr.detectChanges();
+    }, 1000);
+  }
+
+  get tiempoRestante(): string {
+    const min = this.minutos.toString().padStart(2, '0');
+    const seg = this.segundos.toString().padStart(2, '0');
+    return `${min}:${seg}`;
+  }
 }
 
