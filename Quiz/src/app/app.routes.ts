2fe3a0f32065974fbe9e05b4d68f79da384e1b2d
ramaXD870 <jeremiasvillarruelsantos@gmail.com>
2025-10-16 16:52:36 -0300

    cargada

--- Quiz/src/app/app.routes.ts
+++ Quiz/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { Perdiste } from './pages/perdiste/perdiste';
 import { Preguntas } from './pages/preguntas/preguntas';
+import { Inicio } from './pages/inicio/inicio';
+import { Contacto } from './pages/contacto/contacto';
 
 
 export const routes: Routes = [
-    {path: "",component: Preguntas},
+    {path: "",component: Inicio},
+    {path: "inicio", component: Inicio},
     {path: "perdiste",component: Perdiste},
-    {path: "preguntas", component: Preguntas}
+    {path: "preguntas", component: Preguntas},
+    {path: "contacto", component: Contacto}
 ];
